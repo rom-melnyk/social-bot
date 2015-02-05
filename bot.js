8ae6e26aae9523046d8f8d9b0b80d49cc333e587
Roman Melnyk <email.rom.melnyk@gmail.com>
2015-02-05 22:59:36 +0200

    Express app

--- bot.js
+++ bot.js
@@ -1,1 +1,19 @@
-console.log('Running');
+var express = require('express'),
+	app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// ------------ routes ------------
+app.get('/', function (req, res) {
+	res.send('The bot is running');
+});
+
+// ------------ the server itself ------------
+var server = app.listen(3000, function () {
+	var host = server.address().address,
+		port = server.address().port;
+
+	console.log('The bot is running and listening the http://%s:%s', host, port);
+
+});
+
