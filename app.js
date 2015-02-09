98fb59d7ba9614f2ba3973acb25d0527994d12d1
olehbr29 <olehbr29@gmail.com>
2015-02-09 13:35:43 +0200

    moved UI related files to "public" folder. removed unnecessary files

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -21,7 +20,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
