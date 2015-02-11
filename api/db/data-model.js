6bece2d315c16fe2c460a8e5da5a07e63874f2fa
Roman Melnyk <email.rom.melnyk@gmail.com>
2015-02-11 14:57:54 +0200

    the Data model added

---
+++ api/db/data-model.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Data', {
+	url: String,
+	type: String,					// the human-friendly wording for the API call
+	payload: Mixed,					// the payload
+	date: Date,
+	network: String					// one of "fb" or "vk"
+});
