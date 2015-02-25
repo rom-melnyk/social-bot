a6ac1064225617bbaf9f3b128f20d43a84cae80f
olehbr29 <olehbr29@gmail.com>
2015-02-25 11:08:11 +0200

    vk crawler provided

--- api/db/connect.js
+++ api/db/connect.js
@@ -2,6 +2,7 @@ var cfg = require('../config'),
 	mongoose = require('mongoose');
 
 var dbUrl = 'mongodb://' + cfg.db.host + '/' + cfg.db.name;
+console.log(dbUrl);
 mongoose.connect(dbUrl);
 
 mongoose.connection.on('error', function () {
