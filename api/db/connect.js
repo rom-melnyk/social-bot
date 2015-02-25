5dba5c5b9a06581dbfdc3be8b4f8021be6ed3a40
olehbr29 <olehbr29@gmail.com>
2015-02-25 12:10:36 +0200

    db user/pass provided

--- api/db/connect.js
+++ api/db/connect.js
@@ -1,7 +1,7 @@
 var cfg = require('../config'),
 	mongoose = require('mongoose');
 
-var dbUrl = 'mongodb://' + cfg.db.host + '/' + cfg.db.name;
+var dbUrl = 'mongodb://' + cfg.db.user + ':' + cfg.db.password + '@' + cfg.db.host + '/' + cfg.db.name;
 var opts = {};
 if (cfg.db.user) {
     opts.user = cfg.db.user;
@@ -9,7 +9,7 @@ if (cfg.db.user) {
 if (cfg.db.password) {
     opts.password = cfg.db.password;
 }
-mongoose.connect(dbUrl, opts);
+mongoose.connect(dbUrl);
 
 mongoose.connection.on('error', function () {
 	throw new Error('Database connection failed');
