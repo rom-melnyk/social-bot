2193e21479cefb01272074d3d6dc37983bf186d9
olehbr29 <olehbr29@gmail.com>
2015-03-05 21:52:09 +0200

    fix for fb api

--- public/javascripts/src/facebookAPI.js
+++ public/javascripts/src/facebookAPI.js
@@ -1,19 +1,9 @@
-/**
- * Created by obryl on 2/5/2015.
- */
-FB.init({
-    appId      : '808363195905708',
-    cookie     : true,  // enable cookies to allow the server to access
-    // the session
-    version    : 'v2.1', // use version 2.1,
-    channelUrl : "http://social-monitoring.herokuapp.com/"
+$(document).ready(function() {
+  $.ajaxSetup({ cache: true });
+  $.getScript('//connect.facebook.net/en_UK/all.js', function(){
+    FB.init({
+      appId: '808363195905708',
+      version: 'v2.1'
+    });
+  });
 });
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
