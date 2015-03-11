94e03c95dea3d32306283d84c98689e49d3523b8
olehbr29 <olehbr29@gmail.com>
2015-03-11 14:34:38 +0200

    mailer sends mails to all active users

--- api/actions/mailer.js
+++ api/actions/mailer.js
@@ -1,4 +1,5 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer'),
+	User = require('../db/user-model');
 
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
@@ -14,7 +15,7 @@ var transporter = nodemailer.createTransport({
 
 // setup e-mail data with unicode symbols
 var createMailBody = function (data) {
-    var htmlTemplate = '';
+    var htmlTemplate = '', userEmails = [];
     data.forEach(function (item) {
         if (item.network === 'vk') {
             htmlTemplate += "Count: " + item.found + "<div style='text-overflow:ellipsis; max-width: 500px; white-space: nowrap; overflow: hidden; max-height: 16px;'> Post: " + item.instance.text + "</div><br/> network: " + item.network + "<br>" +
@@ -26,22 +27,29 @@ var createMailBody = function (data) {
                 "-----------------------------------------------------------<br>";
         }
     });
-    return mailOptions = {
-        from: 'Social Bot ✔', // sender address
-        to: 'olehbr29@gmail.com,trys2016@gmail.com', // list of receivers
-        subject: 'Social bot notification', // Subject line
-        //text: "Post: " + data[0].instance + " network: " + data[0].network, // plaintext body
-        html: htmlTemplate // html body
-    };
+    User.find({}, function (err, users) {
+        if (!err && users) {
+            users.forEach(function (usr) {
+                userEmails.push(usr.email);
+            });
+            mailOptions = {
+                from: 'Social Bot ✔', // sender address
+                to: userEmails.join(','), // list of receivers
+                subject: 'Social bot notification', // Subject line
+                //text: "Post: " + data[0].instance + " network: " + data[0].network, // plaintext body
+                html: htmlTemplate // html body
+            };
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Message sent: ' + JSON.stringify(info));
+                }
+            });
+        }
+    });
 }
 // send mail with defined transport object
 module.exports = function (dataArray) {
     var mailOptions = createMailBody(dataArray);
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Message sent: ' + JSON.stringify(info));
-        }
-    });
 };
