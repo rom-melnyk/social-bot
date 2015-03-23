5357bb89ef4113215c316fc402a9f84d785ce36d
olehbr29 <olehbr29@gmail.com>
2015-03-24 00:13:05 +0200

    db changes

--- public/javascripts/controllers/fb.js
+++ public/javascripts/controllers/fb.js
@@ -12,63 +12,61 @@ angular.module('SocialApp.controllers', []).
                 }
             });
         };
-        $rootScope.$on('userSet', function () {
-            var accessToken, uid,
-                getLoginAccess = function () {
-                    $http.get('/api/state/fb').success(function (response) {
-                        if (response.token && response.state !== "auth-fail") {
-                            $scope.state = response.state;
-                            if (response.state !== "running") {
-                                $scope.crawlerText = "Start FB crawler";
-                            } else {
-                                $scope.crawlerText = "Stop FB crawler";
-                            }
-                            accessToken = response.token;
-                            var refreshToken = function () {
-                                $http.jsonp('/auth?callback=JSON_CALLBACK').success(function (resp) {
-                                    console.log(resp);
-                                });
-                            };
-                            if (!$rootScope.fbInterval) {
-                                $rootScope.fbInterval = setInterval(refreshToken, 5000000);
-                                refreshToken();
-                            }
-                            getGroups();
+        var accessToken, uid,
+            getLoginAccess = function () {
+                $http.get('/api/state/fb').success(function (response) {
+                    if (response.token && response.state !== "auth-fail") {
+                        $scope.state = response.state;
+                        if (response.state !== "running") {
+                            $scope.crawlerText = "Start FB crawler";
                         } else {
-                            FB.getLoginStatus(function (resp) {
-                                if (resp.authResponse && resp.authResponse.accessToken) {
-                                    processFB(resp);
-                                } else {
-                                    FB.login(processFB);
-                                }
-                            });
+                            $scope.crawlerText = "Stop FB crawler";
                         }
-                    });
-                }, processFB = function(response) {
-                accessToken = response.authResponse && response.authResponse.accessToken;
-                getGroups();
-                $scope.loading = false;
-                $http.put('/api/state/fb', {
-                    token: accessToken
-                }).success(function (response) {
-                    console.log('success state save');
+                        accessToken = response.token;
+                        var refreshToken = function () {
+                            $http.jsonp('/auth?callback=JSON_CALLBACK').success(function (resp) {
+                                console.log(resp);
+                            });
+                        };
+                        if (!$rootScope.fbInterval) {
+                            $rootScope.fbInterval = setInterval(refreshToken, 5000000);
+                            refreshToken();
+                        }
+                        getGroups();
+                    } else {
+                        FB.getLoginStatus(function (resp) {
+                            if (resp.authResponse && resp.authResponse.accessToken) {
+                                processFB(resp);
+                            } else {
+                                FB.login(processFB);
+                            }
+                        });
+                    }
                 });
-            };
-            getLoginAccess();
-            $scope.startCrawler = function (event) {
-                if ($scope.state !== "running") {
-                    $http.get('/api/start/fb').success(function (response) {
-                        $scope.state = "running";
-                        $scope.crawlerText = "Stop FB crawler";
-                    });
-                } else {
-                    $http.get('/api/stop/fb').success(function (response) {
-                        $scope.state = "stopped";
-                        $scope.crawlerText = "Start FB crawler";
-                    });
-                }
-            };
-        });
+            }, processFB = function(response) {
+            accessToken = response.authResponse && response.authResponse.accessToken;
+            getGroups();
+            $scope.loading = false;
+            $http.put('/api/state/fb', {
+                token: accessToken
+            }).success(function (response) {
+                console.log('success state save');
+            });
+        };
+        getLoginAccess();
+        $scope.startCrawler = function (event) {
+            if ($scope.state !== "running") {
+                $http.get('/api/start/fb').success(function (response) {
+                    $scope.state = "running";
+                    $scope.crawlerText = "Зупинити моніторинг";
+                });
+            } else {
+                $http.get('/api/stop/fb').success(function (response) {
+                    $scope.state = "stopped";
+                    $scope.crawlerText = "Запустити моніторинг";
+                });
+            }
+        };
         $rootScope.$on('groupsChanged', function () {
             getGroups();
         });
